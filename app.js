ac7560a00fd859696e6bd9c30f04d4730ae31dfa
Rajaabpro <rajaab.pro@gmail.com>
2025-07-26 11:47:50 +0500

    Implement level progression and button flashing in app.js

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 let gameSeq = [];
 let userSeq = [];
 
+let btns = ["yellow", "green", "red", "blue"];
+
 let started = false;
 
 let level = 0;
+
+let h2 = document.querySelector("h2");
 
 document.addEventListener("keypress", function() {
     if (started == false) {
         console.log("game is started");
         started = true;
+        levelUp();
     }
 });
+
+function btnFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function() {
+        btn.classList.remove("flash");
+    }, 1000);   
+}
+
+function levelUp() {
+    level ++;
+    h2.innerText = `Level ${level}`;
+
+
+    let randmIdx = Math.floor(Math.random() * 3);
+    let randColor = btns[randmIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    console.log(randBtn);
+    console.log(randColor);
+    console.log(randmIdx);
+    btnFlash(randBtn);
+}
+
+
